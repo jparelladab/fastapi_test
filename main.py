91d0d20eb6a13d84a75f947176b2fd69ca441363
AVIRE\Joan.Parellada <Joan.Parellada@avire-global.com>
2021-03-25 18:05:21 +0100

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +8,41 @@ app = FastAPI()
 @app.get("/")
 def index():
     return { 'data' : {'name' : 'Joan'}}
+
+@app.get("/blog")
+#to use the query parameters
+def query(limit=10, published:bool = True):
+    if published:
+        return { 'data' : f'{limit} published articles'}
+    else:
+        return {'data' : f'{limit} articles'}
+
+
+
+@app.get("/blog/unpublished")
+def show():
+    return { 'data' : 'all unpublished articles'}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return { 'data' : {'id' : id}}
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published: Optional[bool]
+
+class SuperBlog(Blog):
+    superTitle:str
+    superBody:str
+
+@app.post('/superblog')
+def create_blog(request: SuperBlog):
+    return request
+
+@app.get('/superblog2/{request}')
+def show_blog(request: str):
+    hola = 'holation'
+    adeo = 'adeuu'
+    return { 'data' : 'blog' }
