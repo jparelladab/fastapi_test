eb957435bc6c49c82f457b0b9aaf309dd23374ce
joan parellada <joan.parellada789@gmail.com>
2021-03-31 00:40:41 +0200

    metadata docs

--- blog/main.py
+++ blog/main.py
@@ -5,8 +5,29 @@ from . import models, schemas, hashing
 from typing import List
 
 
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "blogs",
+        "description": "Manage blogs. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "blogs external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
 
-app = FastAPI()
+app = FastAPI(
+    title="My Super Project",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="2.5.0",
+    openapi_tags=tags_metadata
+)
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,7 +42,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
+@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=["blogs"])
 # def create(request: schemas.Blog, db: Session = Depends(get_db)):
 # remove sessionlocal() and db.close from function below
 def create(request: schemas.Blog):
@@ -33,7 +54,7 @@ def create(request: schemas.Blog):
     db.close()
     return new_blog
 
-@app.get('/blogs', response_model=List[schemas.ShowBlog])
+@app.get('/blogs', response_model=List[schemas.ShowBlog], tags=["blogs"])
 # def all(db: Session = Depends(get_db)):
 def all():
     db = SessionLocal()
@@ -41,7 +62,7 @@ def all():
     db.close()
     return blogs
 
-@app.get('/blog/{id}', response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', response_model=schemas.ShowBlog, tags=["blogs"])
 # def show(id, db: Session = Depends(get_db)):
 def show(id, response: Response):
     db = SessionLocal()
@@ -53,7 +74,7 @@ def show(id, response: Response):
     db.close()
     return blog
 
-@app.delete('/blog/{id}')
+@app.delete('/blog/{id}', tags=["blogs"])
 def destroy(id, response: Response):
     db = SessionLocal()
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -65,7 +86,7 @@ def destroy(id, response: Response):
     db.close()
     return {'detail': f'blog with id {id} deleted'}
 
-@app.put('/blog/{id}')
+@app.put('/blog/{id}', tags=["blogs"])
 def update(id, request: schemas.Blog, response: Response):
     db = SessionLocal()
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -79,7 +100,7 @@ def update(id, request: schemas.Blog, response: Response):
 
 
 
-@app.post('/user/')
+@app.post('/user/', response_model=schemas.ShowUser, tags=["users"])
 def create_user(request: schemas.User):
     db = SessionLocal()
     new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
@@ -88,6 +109,25 @@ def create_user(request: schemas.User):
     db.refresh(new_user)
     db.close()
     return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=["users"])
+def get_user(id):
+    db = SessionLocal()
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'details' : f'user with id {id} not found'}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id {id} not found')
+    db.close()
+    return user
+
+@app.get('/users', tags=["users"])
+def all_users():
+    db = SessionLocal()
+    users = db.query(models.User).all()
+    db.close()
+    return users
+
 
 
 
--- blog/schemas.py
+++ blog/schemas.py
@@ -13,3 +13,9 @@ class User(BaseModel):
     name:str
     email:str
     password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    class Config:
+        orm_mode = True
